5dc603d494cce4e5e9675e0e9cfbc1b99ef2bb29
Reverse7K <zakoki711@gmail.com>
2025-01-21 10:16:19 +0700

    update

--- app/page1/page.tsx
+++ app/page1/page.tsx
@@ -6,6 +6,7 @@ function page() {
         <div>
             <h1 className="text-4xl text-center" >This is Page 1</h1>
             <Image src="/cr7.jpg" alt="Cr7" width={400} height={400} />
+            <h2>66162110113-9</h2>
             <div><Link href="/page2" >go to page 2</Link></div>
             <div><Link href="/page3" >go to page 3</Link></div>
          </div>
--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -6,6 +6,7 @@ function page() {
         <div>
             <h1 className="text-4xl text-center">This is Page 2</h1>
             <Image src="/lamine.jpg" alt="Cr7" width={400} height={400} />
+            <h2>66162110113-9</h2>
             <div><Link href="/page1">back to page 1</Link></div>
             <div><Link href="/page3">go to page 3</Link></div>
         </div>
